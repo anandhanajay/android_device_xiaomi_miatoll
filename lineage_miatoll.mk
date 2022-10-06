4e63235f4bd714db41d8eaa15b0a089252445a37
anandhanajay <68692677+anandhanajay@users.noreply.github.com>
2022-10-06 14:35:34 +0530

    Update lineage_miatoll.mk

--- lineage_miatoll.mk
+++ lineage_miatoll.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+#RiceDroid Maintainer Flags.
+RICE_MAINTAINER := anandhanajay
+
 # Inherit from miatoll device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
